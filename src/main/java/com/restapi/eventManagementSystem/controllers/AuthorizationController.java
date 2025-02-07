2b5e4f9dad23804e8251b7e1b0d4388579f53877
VeeraOffice <veeravinu345@gmail.com>
2025-02-07 23:14:08 +0530

    kk

---
+++ src/main/java/com/restapi/eventManagementSystem/controllers/AuthorizationController.java
@@ -0,0 +1,25 @@
+package com.restapi.eventManagementSystem.controllers;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorizationController 
+{
+	
+	@GetMapping("/api/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> checking()
+	{
+		return ResponseEntity.ok(
+	             Map.of("status", HttpStatus.OK.value(), "message", "Authorized")
+	         );
+		
+	}
+
+}
